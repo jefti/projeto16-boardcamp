853575e907f3c55b25a79a37486d078866fcc11f
jefti <jeftimeira@gmail.com>
2023-07-26 23:42:36 -0300

    feature: Midllewares

--- src/middlewares/validateRentals.middleware.js
+++ src/middlewares/validateRentals.middleware.js
@@ -1,9 +1,9 @@
-import { customersSchema } from "../schemas/customer.schemas.js";
+import { rentalsSchema } from "../schemas/rentals.schema.js";
 
-export async function validateCustomers(req, res, send){
+export async function validateRentals(req, res, send){
     try{
         if(!req.body) return res.send("Necessário body").status(204);
-        const validation = customersSchema.validate(req.body,{abortEarly: false});
+        const validation = rentalsSchema.validate(req.body,{abortEarly: false});
         if(validation.error){
             const errors = validation.error.details.map((d)=>d.message);
             return res.send(errors).status(400);
